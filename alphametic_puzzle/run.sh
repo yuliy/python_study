5f2fb06e7ab9f3731e3c1469b5d52aa2f55f4ca6
Yuliy Ayvazyan <yuliy@yuliy.me>
2019-05-11 01:05:24 +0300

    solve_alphametic_puzzle tool implementation

---
+++ alphametic_puzzle/run.sh
@@ -0,0 +1,9 @@
+
+echo '___________________________________________________'
+time python3 solve_alphametic_puzzle.py "HAWAII + IDAHO + IOWA + OHIO == STATES"
+
+echo '___________________________________________________'
+time python3 solve_alphametic_puzzle.py "I + LOVE + YOU == DORA"
+
+echo '___________________________________________________'
+time python3 solve_alphametic_puzzle.py "SEND + MORE == MONEY"
