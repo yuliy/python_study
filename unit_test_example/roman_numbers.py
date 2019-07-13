8dff07bb88950646d546f10f783a9288eaf7c326
Yuliy Ayvazyan <yuliy@yuliy.me>
2019-07-14 01:04:52 +0300

    some more test cases

--- unit_test_example/roman_numbers.py
+++ unit_test_example/roman_numbers.py
@@ -21,8 +21,8 @@ roman_numeral_map = (
 
 def to_roman(n):
     '''convert integer to Roman numberal'''
-    if n > 3999:
-        raise OutOfRangeError('number out of range (must be less than 4000)')
+    if not (0 < n < 4000):
+        raise OutOfRangeError('number out of range (must be 1..3999)')
 
     result = ''
     for numeral, integer in roman_numeral_map:
