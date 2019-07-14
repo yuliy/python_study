f76e72b24f682a1fc8240cf8f1c720e7dd89b5c0
Yuliy Ayvazyan <yuliy@yuliy.me>
2019-07-14 21:22:59 +0300

    context manager as a generator

--- with_statement/main.py
+++ with_statement/main.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
-class Foo(object):
+from contextlib import contextmanager
+
+
+'''
+    The usual way of implementing a context manager
+'''
+class File(object):
     def __init__(self, file_name, method):
         print('__init__')
         self.file_obj = open(file_name, method)
@@ -15,9 +21,24 @@ class Foo(object):
         self.file_obj.close()
 
 
+'''
+    Implementing a context manager as a generator
+'''
+@contextmanager
+def open_file(name, method):
+    f = open(name, method)
+    yield f
+    f.close()
+
+
 def main():
-    with Foo('blabla.txt', 'r') as foo:
+    print('====================================')
+    with File('blabla.txt', 'r') as foo:
         print('inside with statement')
+
+    print('====================================')
+    with open_file('blabla.txt', 'r') as foo:
+        print('inside other with statement')
 
 
 if __name__ == '__main__':
