a4d032a114ba25c78141fe32c001c77698f4a667
Yuliy Ayvazyan <yuliy@yuliy.me>
2019-07-16 01:46:15 +0300

    @

--- xml_example/main.py
+++ xml_example/main.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
+import xml.etree.ElementTree as etree
+
 
 def main():
-    pass
+    tree = etree.parse('sample.xml')
+    root = tree.getroot()
+    print(root)
 
 
 if __name__ == '__main__':
