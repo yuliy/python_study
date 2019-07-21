5aa336b15c9631ca009d7f869dc3644a280400ee
Yuliy Ayvazyan <yuliy@yuliy.me>
2019-07-21 22:37:36 +0300

    @

--- http_with_httplib2/main.py
+++ http_with_httplib2/main.py
@@ -4,12 +4,30 @@ import httplib2
 httplib2.debuglevel = 1
 
 
+def print_response(response, content):
+    print( F'status: {response.status}' )
+    print( F'content len: {len(content)}' )
+    print( F'cached: {response.fromcache}' )
+
+
 def main():
     h = httplib2.Http('.cache')
+
+    # response 1
+    print( '_____________________________________' )
     response, content = h.request('http://lovioblaka.me')
-    print( response.status )
-    print( len(content) )
-    #print( content.decode('utf-8') )
+    print_response(response, content)
+
+    # response 2
+    print( '_____________________________________' )
+    response, content = h.request('http://lovioblaka.me')
+    print_response(response, content)
+
+    # response 3
+    print( '_____________________________________' )
+    response, content = h.request('http://lovioblaka.me',
+        headers={'cache-control': 'no-cache'})
+    print_response(response, content)
 
 
 if __name__ == '__main__':
