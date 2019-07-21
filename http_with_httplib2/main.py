1f8a2a59b548347f9a5d4fb1435a014097ba0a46
Yuliy Ayvazyan <yuliy@yuliy.me>
2019-07-21 22:09:10 +0300

    @

--- http_with_httplib2/main.py
+++ http_with_httplib2/main.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
-
+import httplib2
 
 
 def main():
-    pass
+    h = httplib2.Http('.cache')
+    response, content = h.request('http://yuliy.me')
+    print( response.status )
+    print( len(content) )
 
 
 if __name__ == '__main__':
