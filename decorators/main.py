119e32e8e1f27b1d6f064a4f80d3fbac81e6911c
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2021-02-04 19:31:57 +0300

    @

--- decorators/main.py
+++ decorators/main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+"""
+Simple decorators
+"""
 
 def bread(func):
     def wrapper():
@@ -23,8 +26,24 @@ def sandwich(food='--ham--'):
     print food
 
 
+"""
+Decorators with parameters
+"""
+
+def a_decorator_passing_arguments(function_to_decorate):
+    def a_wrapper_accepting_arguments(arg1, arg2):
+        print "Look what I've got:", arg1, arg2
+        function_to_decorate(arg1, arg2)
+    return a_wrapper_accepting_arguments
+
+@a_decorator_passing_arguments
+def print_full_name(first_name, last_name):
+    print "My name is", first_name, last_name
+
+
 def main():
     sandwich()
+    print_full_name("Sam", "Smith")
 
 if __name__ == '__main__':
     main()
