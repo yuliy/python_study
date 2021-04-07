1eabc01e65ceb907a3fa001a7c985ca8c353d082
Yuliy Ayvazyan <yuliy@yandex-team.ru>
2021-04-07 18:00:48 +0300

    @

--- logging/main.py
+++ logging/main.py
@@ -2,9 +2,13 @@
 # encoding: utf-8
 
 import logging
+#logging.setLevel(logging.DEBUG)
+logging.basicConfig(filename='example.log', level=logging.DEBUG)
 
 def main():
-    logging.info('hello world!')
+    logging.debug('my debug message')
+    logging.error('my error message')
+    logging.info('my info message')
 
 
 if __name__ == '__main__':
